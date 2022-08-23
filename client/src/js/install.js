7d83b9dfe519e194db758d1ac3d769ee2d7ff718
LSton40 <logan.sutton@gmail.com>
2022-08-23 07:41:39 -0500

    Troubleshooting

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -5,37 +5,51 @@ let deferredPrompt;
 // TODO: Add an event handler to the `beforeinstallprompt` event
 
 window.addEventListener('beforeinstallprompt', (event) => {
-    event.preventDefault();
-    deferredPrompt = event;
+    // event.preventDefault();
+    
+    window.deferredPrompt = event;
+
+    // butInstall.classList.toggle('hidden', false);
 
     // butInstall.hidden = false;
     // appinstalled();
 
-    event.userChoice.then((result) => {
-        console.log(result.outcome);
-    }, handleError)
+    // event.userChoice.then((result) => {
+    //     console.log(result.outcome);
+    // }, handleError)
 });
 
 // TODO: Implement a click event handler on the `butInstall` element
 butInstall.addEventListener('click', async () => {
-    deferredPrompt.prompt();
-    // butInstall.disabled = true;
 
-    const {outcome} = await deferredPrompt.userChoice;
+    const prompt = window.deferredPrompt;
 
-    if (outcome === 'accepted') {
-        console.log('User accepted the install prompt.');
-        // butInstall.hidden = true;
-    } else if (outcome === 'dismissed') {
-        console.log('User dismissed the install prompt.');
+    if (!prompt) {
+        return;
     }
 
+    prompt.prompt();
+    // butInstall.disabled = true;
+
+    // const {outcome} = await deferredPrompt.userChoice;
+
+    // if (outcome === 'accepted') {
+    //     console.log('User accepted the install prompt.');
+    //     // butInstall.hidden = true;
+    // } else if (outcome === 'dismissed') {
+    //     console.log('User dismissed the install prompt.');
+    // }
+
     // butInstall.disabled = false;
-    deferredPrompt = null;
+    window.deferredPrompt = null;
+
+    // butInstall.deferredPrompt = null;
 
 });
 
 // TODO: Add an handler for the `appinstalled` event
 window.addEventListener('appinstalled', (event) => {
+
+    window.deferredPrompt = null;
     console.log('Thank you for installing our app!', event);
 });
